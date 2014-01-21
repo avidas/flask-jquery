ce39f218fd72a0f551c86609d34acaa3cfb38995
Avi Das <adas@colgate.edu>
2014-01-20 18:39:58 -0600

    draggable with jquery ui, keypress animate

---
+++ static/js/ui.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	$("#calculator").draggable();
+
+	$("#calculator").dblclick(function(){
+        $(this).html("<h1>42</h1>");
+    });
+
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+			case 65:
+				$("#calculator").animate({left: "-=10px"}, 'fast');
+				break;
+			case 83:
+				$("#calculator").animate({up: "-=10px"}, 'fast');
+				break;
+			case 87:
+				$("#calculator").animate({up: "+=10px"}, 'fast');
+				break;
+			case 68:
+				$("#calculator").animate({left: "+=10px"}, 'fast');
+				break;
+			default:
+				break;
+		}
+	});
+});
