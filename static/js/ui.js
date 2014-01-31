269c736a8846fda78cce6ace9feec90e45e6ed20
Avi Das <adas@colgate.edu>
2014-01-30 21:12:34 -0600

    Add tooltip

--- static/js/ui.js
+++ static/js/ui.js
@@ -2,11 +2,21 @@ $(document).ready(function () {
 
 	$(" .container ").hover(function(){
 		$(this).addClass("hover_emp");	
-		$(this).append("<p class='prompt'>Just a paragraph.</p>");
+		//$(this).append("<p class='prompt'>Just a paragraph.</p>");
 	}, function() {
 		$(this).removeClass("hover_emp");
-		$('p').remove(".prompt");
+		//$('p').remove(".prompt");
 	});
+
+	$('p').click(function() {
+		//$(this).append( $("#comment") ).css('visibility: visible');
+		$(this).append("<div id='tooltip'><input type='text' name='comment' />");
+		var toAdd = $('input[name=comment]').val();
+		$('p').append('<div class="item">' + toAdd + '</div>');
+		//<input type='button' value='Insert' />
+		//$('#tooltip').css({'position': 'absolute', 'top': $('p').position().top + 'px', 'left': $('p').position().left + 'px'});
+	});
+
 
 	$(".calculator").hide().fadeIn(1000);
 
