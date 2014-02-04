61ca6a28f534584db59595cdfdcd27eab328bd56
Avi Das <adas@colgate.edu>
2014-02-03 20:42:11 -0600

    Message for browsers not supporting canvas

--- static/js/snake.js
+++ static/js/snake.js
@@ -1,8 +1,10 @@
-$(document).ready(function () {
-
-	//Canvas attributes
+$(document).ready(function(){
+	//Canvas stuff
 	var canvas = $("#canvas")[0];
 	var ctx = canvas.getContext("2d");
 	var w = $("#canvas").width();
 	var h = $("#canvas").height();
+	
+	ctx.fillStyle = "blue";
+
 })
