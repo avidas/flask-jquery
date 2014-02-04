fc1c5b98744505fb06a68963e6039a9a8bdf6827
Avi Das <adas@colgate.edu>
2014-02-03 20:31:37 -0600

    Get canvas attributes for snake

---
+++ static/js/snake.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+	//Canvas attributes
+	var canvas = $("#canvas")[0];
+	var ctx = canvas.getContext("2d");
+	var w = $("#canvas").width();
+	var h = $("#canvas").height();
+})
