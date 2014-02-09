9167faf9078a270cabece5940f3bfd433cbaed74
Avi Das <adas@colgate.edu>
2014-02-09 15:07:25 -0600

    Make snake move and make food stay in the same location despite canvas redraw

--- static/js/snake.js
+++ static/js/snake.js
@@ -6,9 +6,10 @@ $(document).ready(function(){
 	var h = $("#canvas").height();
 	
 	var cw = 10;
-	var dir;
-	var score;
 	var snake=[]; //array representing snake coordinates
+
+	var fw = Math.floor((Math.random()*w/10)+1);
+	var fh = Math.floor((Math.random()*h/10)+1);
 
 	//Draw canvas
 	ctx.fillStyle = "white";
@@ -16,11 +17,10 @@ $(document).ready(function(){
 	ctx.strokeStyle = "black";
 	ctx.strokeRect(0, 0, w, h);
 
-
 	function paint_cell(x,y)
 	{
 		ctx.fillStyle = "blue";
-		console.log(x,y);
+		//console.log(x,y);
 		ctx.fillRect(x*cw, y*cw, cw, cw);
 		ctx.strokeStyle = "white";
 		ctx.strokeRect(x*cw, y*cw, cw, cw);
@@ -29,33 +29,68 @@ $(document).ready(function(){
 	function make_snake()
 	{
 		var snake_length = 5; //starting length of snake
-		for(var i=0;i<5;i++)
+		for(var i=0;i<snake_length;i++)
 		{
 			//Create horizontal snake from top left
 			snake.push({x: i,y: 0});
-			paint_cell(snake[i].x,snake[i].y)
+			paint_cell(snake[i].x,snake[i].y);
 		}
 	}
 
-	console.log(snake);
-	for(var i=0;i<snake.length;i++)
+	function draw_snake()
 	{
-		paint_cell(snake[i].x,snake[i].y)
+		//console.log(snake.length);
+		//console.log(snake);
+		for(var i = snake.length-1; i >= 0; i--)
+		{
+			console.log(snake[i]);
+			paint_cell(snake[i].x,snake[i].y);
+		}
 	}
+
+	function game_logic()
+	{
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, w, h);
+		ctx.strokeStyle = "black";
+		ctx.strokeRect(0, 0, w, h);
+
+		//console.log(snake[snake.length-1].x,snake[snake.length-1].y);
+		snake.shift();
+		//console.log("this ", snake[snake.length-1].x + 1, snake[snake.length-1].y)
+		//paint_cell(snake[snake.length-1].x + 1, snake[snake.length-1].y)
+
+		
+		snake.push({x: snake[snake.length-1].x + 1, y: snake[snake.length-1].y});
+		make_food();
+		draw_snake();
+	}
+
 
 	function game()
 	{
-		setInterval(make_snake(), 60);
-		make_food();
+		make_snake();
+		
+		if (snake[snake.length-1].x >= w)
+		{
+			clearInterval();
+			game();
+		}
+		var handle = setInterval(game_logic, 500);
+
 	}
 	game();
     
-    function make_food()
+    function make_food(is_new)
     {
-        fw = Math.floor((Math.random()*w/10)+1);
-        fh = Math.floor((Math.random()*h/10)+1);
-        console.log(fw,fh);
+    	console.log(fw);
+        fw = typeof is_new !== 'undefined' ? Math.floor((Math.random()*w/10)+1) : fw;
+        console.log("here " + fw);
+        fh = typeof is_new !== 'undefined' ? Math.floor((Math.random()*h/10)+1) : fh;
+        //console.log(fw,fh);
         paint_cell(fw,fh);
     }
+
+    //$(document).keydown(e)
 	
 })
